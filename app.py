b7cd4e184bce975e77ba98f7837e5b81acf5882c
rodims-code <ridrodims@hotmail.com>
2025-03-12 16:38:50 +0100

    return name via route url

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     return "Hello wlecom ot my site web"
+
+@app.route("/bonjour/<string:name>")
+def bonjour(name : str):
+    return f"bonjour {name}"
